2a33428fefca5799b4c37d1e73738c5bc5001ab4
otegal <gyouzakuuka@gmail.com>
2022-11-13 14:56:56 +0900

    ブラウザ自動操作をRustで試してみたかったので、rust-headless-chromeを入れて阿部寛のホームページのスクショをとってみた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use headless_chrome::{protocol::cdp::Page, Browser};
+use std::{fs, thread};
+
+fn main() -> anyhow::Result<()> {
+    let browser = Browser::default()?;
+    let tab = browser.wait_for_initial_tab()?;
+
+    tab.navigate_to("http://abehiroshi.la.coocan.jp/")?
+        .wait_for_xpath("/html/frameset/frame[1]")?
+        .focus()?
+        .wait_for_xpath("html/body/table/tbody/tr[10]/td[3]/p/a")?
+        .click()?
+        .wait_for_xpath("/html/frameset/frame[2]")?
+        .focus()?
+        .wait_for_xpath("/html/body/table/tbody/tr[8]/td[2]/strong/a")?
+        .click()?;
+
+    thread::sleep(std::time::Duration::from_millis(3000));
+
+    let cap_tab =
+        tab.capture_screenshot(Page::CaptureScreenshotFormatOption::Jpeg, None, None, true)?;
+    fs::write("cap_tab.jpeg", &cap_tab)?;
+
+    Ok(())
 }
